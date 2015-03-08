712e73c536d1e939b75ebdc5c8b35c0fce4932ef
mather <mather314@gmail.com>
2015-03-09 01:17:20 +0900

    Pass 3rd example.

---
+++ src/test/scala/com/github/mather/uddf/BiologicalPopulationInventoriesSpec.scala
@@ -0,0 +1,23 @@
+package com.github.mather.uddf
+
+import org.specs2.mutable._
+
+import scala.xml.{Elem, XML}
+
+/**
+ *
+ * @author kuwahataeisuke
+ */
+class BiologicalPopulationInventoriesSpec extends Specification {
+  val xml = XML.load(getClass.getResource("/1_3_biological_population_inventories.xml"))
+
+  "Diver Data XML" >> {
+    "can be load as XML" >> {
+      xml must haveClass[Elem]
+    }
+    "can be parsed as UDDF" >> {
+      val uddf = scalaxb.fromXML[Uddf](xml)
+      uddf must haveClass[Uddf]
+    }
+  }
+}
