2bbaec586571025e581a662af0b8a8c3dda01e4a
mather <mather314@gmail.com>
2015-03-15 23:37:30 +0900

    Pass 8th example
    - Refine dcalarmWithTimeType to adjust example. Setting "datetime" for alarm is nonsense.

---
+++ src/test/scala/com/github/mather/uddf/example/ConfigurationOfADiveComputer.scala
@@ -0,0 +1,26 @@
+package com.github.mather.uddf.example
+
+import com.github.mather.uddf.Uddf
+import org.specs2.mutable._
+
+import scala.xml.{Elem, XML}
+
+
+/**
+ *
+ * @author kuwahataeisuke
+ */
+class ConfigurationOfADiveComputerSpec extends Specification {
+
+  val xml = XML.load(getClass.getResource("/5_1_configuration_of_a_dive_computer.xml"))
+
+  "Diver Data XML" >> {
+    "can be load as XML" >> {
+      xml must haveClass[Elem]
+    }
+    "can be parsed as UDDF" >> {
+      val uddf = scalaxb.fromXML[Uddf](xml)
+      uddf must haveClass[Uddf]
+    }
+  }
+}
